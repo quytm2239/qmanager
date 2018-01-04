8a47bb35f87d1062ca3e18b1d9609f8109551355
quytm2239 <quytm2239@gmail.com>
2018-01-04 17:17:45 +0700

    Init commit

---
+++ QManager/QManager/DBConnection/DBConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace QManager.DBConnection
+{
+    class DBConnection
+    {
+        private MySqlConnectionStringBuilder connectionStringBuilder = new MySqlConnectionStringBuilder();
+
+        private string HOST = "35.201.228.124";
+        private static DBConnection instance;
+        private DBConnection()
+        {
+            connectionStringBuilder.Server = HOST;
+            connectionStringBuilder.Database = Database;
+            connectionStringBuilder.UserID = UserId;
+            connectionStringBuilder.Password = Pass;
+            connectionStringBuilder.MinimumPoolSize = 100;
+        }
+
+        public DBConnection getInstance(){
+            if (instance == null)
+            {
+                instance = new DBConnection();
+            }
+            return instance;
+        }
+    }
+}
---
+++ QManager/QManager/Form1.cs
@@ -0,0 +1,49 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QManager
+{
+    public partial class Form1 : Form
+    {
+        private Form form2 = new Form();
+        public Form1()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+            form2.MdiParent = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("File is clicked!");
+        }
+
+        private void openForm2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.IsMdiContainer = true;
+            if (form2.Visible)
+            {
+                return;
+            }
+            form2.WindowState = FormWindowState.Maximized;
+            form2.Show();
+        }
+    }
+}
