bd06b117aeac073dc2bed997c1660b38d5905d07
quytm2239 <quytm2239@gmail.com>
2018-01-08 00:39:34 +0700

    Rename due to change path

--- QManager/QManager/src/Form1.cs
+++ QManager/QManager/src/Form1.cs
@@ -1,11 +1,4 @@
-﻿ using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace QManager
@@ -23,6 +16,7 @@ namespace QManager
         private void Form1_Load(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Maximized;
+            DBConnection.getInstance();
         }
 
         private void label1_Click(object sender, EventArgs e)
