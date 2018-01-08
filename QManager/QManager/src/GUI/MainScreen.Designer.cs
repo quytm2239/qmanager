4fe4330df63c061b873e94f775c9c6b36bc4a903
quytm2239 <quytm2239@gmail.com>
2018-01-09 00:09:46 +0700

    Update draft

--- QManager/QManager/src/GUI/MainScreen.Designer.cs
+++ QManager/QManager/src/GUI/MainScreen.Designer.cs
@@ -30,31 +30,42 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
             this.leftMenu = new System.Windows.Forms.Panel();
-            this.panelContent = new System.Windows.Forms.Panel();
             this.buttonCloseLeftMenu = new System.Windows.Forms.Button();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.PictureBoxAvatar = new System.Windows.Forms.PictureBox();
             this.leftMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAvatar)).BeginInit();
             this.SuspendLayout();
             // 
             // leftMenu
             // 
             this.leftMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.leftMenu.Controls.Add(this.PictureBoxAvatar);
             this.leftMenu.Controls.Add(this.buttonCloseLeftMenu);
             resources.ApplyResources(this.leftMenu, "leftMenu");
             this.leftMenu.Name = "leftMenu";
             this.leftMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.LeftMenu_Paint);
+            // 
+            // buttonCloseLeftMenu
+            // 
+            resources.ApplyResources(this.buttonCloseLeftMenu, "buttonCloseLeftMenu");
+            this.buttonCloseLeftMenu.FlatAppearance.BorderSize = 0;
+            this.buttonCloseLeftMenu.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonCloseLeftMenu.Name = "buttonCloseLeftMenu";
+            this.buttonCloseLeftMenu.UseVisualStyleBackColor = true;
+            this.buttonCloseLeftMenu.Click += new System.EventHandler(this.buttonCloseLeftMenu_Click);
             // 
             // panelContent
             // 
             resources.ApplyResources(this.panelContent, "panelContent");
             this.panelContent.Name = "panelContent";
             // 
-            // buttonCloseLeftMenu
+            // PictureBoxAvatar
             // 
-            resources.ApplyResources(this.buttonCloseLeftMenu, "buttonCloseLeftMenu");
-            this.buttonCloseLeftMenu.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.buttonCloseLeftMenu.Name = "buttonCloseLeftMenu";
-            this.buttonCloseLeftMenu.UseVisualStyleBackColor = true;
-            this.buttonCloseLeftMenu.Click += new System.EventHandler(this.buttonCloseLeftMenu_Click);
+            this.PictureBoxAvatar.Image = global::QManager.Properties.Resources.friendly_for_facebook;
+            resources.ApplyResources(this.PictureBoxAvatar, "PictureBoxAvatar");
+            this.PictureBoxAvatar.Name = "PictureBoxAvatar";
+            this.PictureBoxAvatar.TabStop = false;
             // 
             // MainScreen
             // 
@@ -67,6 +78,7 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainScreen_Load);
             this.leftMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAvatar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,5 +88,6 @@
         private System.Windows.Forms.Panel leftMenu;
         private System.Windows.Forms.Panel panelContent;
         private System.Windows.Forms.Button buttonCloseLeftMenu;
+        private System.Windows.Forms.PictureBox PictureBoxAvatar;
     }
 }
